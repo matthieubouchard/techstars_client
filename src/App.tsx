ec28e4b1cec271c3c5d0289f97593fbb5c30758b
Matthew Bouchard <matthew.bouchard@cognizant.com>
2020-06-05 00:03:13 -0600

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios'
+import styled from 'styled-components'
+
+import Header from './Header'
+import CompanyCard from './CompanyCard'
+
+const apitest = async () => {
+  const res = await axios.get('http://localhost:4000/founders')
+  console.log('res!', res)
+}
+
+const Layout = styled.div.attrs({ className: "flex w-full justify-center py-10 bg-gray-100" })`
+  padding-left: 5rem;
+  padding-right: 5rem;
+  height: 100vh;
+  padding-top: 8rem;
+`
 
 function App() {
+  apitest()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Layout>
+        <div className="w-8/12 h-">
+          <CompanyCard />
+        </div>
+      </Layout>
+    </>
   );
 }
 
